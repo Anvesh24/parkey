2e7c71180696c774acd9f1a9d76c1aa1c26a67de
Anvesh <anvesh.v@gmail.com>
2015-07-09 20:34:35 +0530

    install devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
