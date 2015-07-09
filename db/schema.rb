f15280803bf4d4537d631c7ba75673f69c81bc53
Anvesh <anvesh.v@gmail.com>
2015-07-09 21:02:17 +0530

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709131705) do
+ActiveRecord::Schema.define(version: 20150709152639) do
+
+  create_table "parkings", force: :cascade do |t|
+    t.integer  "area"
+    t.string   "type"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.float    "latitude"
+    t.float    "longitude"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
